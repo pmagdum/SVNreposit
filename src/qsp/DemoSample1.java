a00d60062dff9aef16e40911598bab5781f8ff5b
pmagdum <pmagdum@gmail.com>
2016-07-10 14:57:44 +0530

    Update DemoSample1.java

--- src/qsp/DemoSample1.java
+++ src/qsp/DemoSample1.java
@@ -5,6 +5,7 @@ public class DemoSample1 {
 	public static void main(String[] args)
 	{
 		System.out.println("This is another class in SVNReposit repository");
+		system.out.println("Check out from repository.");
 
 	}
 
