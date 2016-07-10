150a177aff45ed167cdf8eeb004f4005ebd6030f
pmagdum <pmagdum@gmail.com>
2016-07-10 14:43:31 +0530

    Update DemoSample.java

--- src/qsp/DemoSample.java
+++ src/qsp/DemoSample.java
@@ -5,6 +5,7 @@ public class DemoSample {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is SVNreposit repository.");
+		System.out.println("create your scripts here and save.");
 
 	}
 
